42a010f9b692b72e7d37f8ca01c260f866d2b23f
Juan Cruz Carrizo <juancarrizoast@gmail.com>
2022-07-12 12:12:55 -0300

    fk

--- src/apps/productos/admin.py
+++ src/apps/productos/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Marca
 # Register your models here.
 
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ["id","nombre", "precio", "cantidad", "activo"]
+    list_display = ["id","nombre", "precio", "cantidad", "activo", "marca"]
 
 admin.site.register(Producto, ProductoAdmin)
+
+class MarcaAdmin(admin.ModelAdmin):
+    list_display = ["id","nombre"]
+
+admin.site.register(Marca, MarcaAdmin)
--- src/apps/productos/models.py
+++ src/apps/productos/models.py
@@ -1,16 +1,27 @@
 from pyexpat import model
 from django.db import models
 
-# Create your models here.
+class Marca(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    class Meta:
+        db_table = "marcas"
+    
+    def __str__(self):
+        return self.nombre
+
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=250)
     precio = models.DecimalField(max_digits=9, decimal_places=2)
     cantidad = models.IntegerField(default=0)
     activo = models.BooleanField(default=True)
-    imagen = models.ImageField(upload_to = "productos", null=True)
+    imagen = models.ImageField(upload_to = "productos", null=True, blank=True)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE, related_name="productos_relacionados", null=True)
 
     class Meta:
         db_table = "productos"
 
     def __str__(self):
         return self.nombre
+
