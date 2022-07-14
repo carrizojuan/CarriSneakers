c861031f8a0a56d7d714b577542df410a2838e14
Juan Cruz Carrizo <juancarrizoast@gmail.com>
2022-07-14 10:34:14 -0300

    template tag

---
+++ src/apps/productos/templatetags/tags.py
@@ -0,0 +1,13 @@
+from atexit import register
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def saludar():
+    return "Hola a todos, soy un tag"
+
+
+@register.simple_tag
+def saludar_con_nombre(nombre):
+    return f"Hola soy {nombre}"
--- src/hugoOfemme/settings/base.py
+++ src/hugoOfemme/settings/base.py
@@ -77,6 +77,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'productos_tags': 'apps.productos.templatetags.tags'
+            }
         },
     },
 ]
