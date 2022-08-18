47303411549a31076dc29800404fa455fef35d1b
Juan Cruz Carrizo <juancarrizoast@gmail.com>
2022-08-18 10:13:51 -0300

    eliminacion newsletter

--- src/apps/newsletters/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-""" from .views import newsletter_suscribe
-
-app_name = "newsletters"
-
-urlpatterns = [
-    path("suscribe/", newsletter_suscribe, name="newsletter_suscribe"),   
-] """
